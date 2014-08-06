d60057bda2d9af52188fcc6f3ea206bf1f36e5cb
georg.tiefenbrunn <georg.tiefenbrunn@gmail.com>
2014-08-06 08:24:06 +0200

    [TASK] add connection to EXT:news; fixes #32

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,3 +8,21 @@ CREATE TABLE sys_file_collection (
 	sorting int(11) DEFAULT '0' NOT NULL,
 	KEY parentalbum (parentalbum)
 );
+
+#
+# Table structure for table 'tx_news_domain_model_news'
+#
+CREATE TABLE tx_news_domain_model_news (
+	related_fsmediaalbums tinytext NULL,
+);
+
+#
+# Table structure for table 'tx_news_domain_model_news_fsmediaalbums_mm'
+#
+CREATE TABLE tx_news_domain_model_news_fsmediaalbums_mm (
+	uid_local int(11) DEFAULT '0' NOT NULL,
+	uid_foreign int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
+);
