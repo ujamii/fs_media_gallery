760ec3fc612254fef4f390c7fe186fa5745d3cfa
Frans Saris <frans@beech.it>
2015-12-19 09:23:22 +0100

    [BUGFIX] Reverted PSR-2 change of News class
    EXT:news 3.2.* can not handle extenting the PSR-2 formatted classe yet.
    
    To be compatible with EXT:news 3.2.* and EXT:news 4.* the PSR-2
    formatting is removed for this specific class.

--- Classes/Domain/Model/News.php
+++ Classes/Domain/Model/News.php
@@ -14,8 +14,7 @@ namespace MiniFranske\FsMediaGallery\Domain\Model;
 /**
  * News
  */
-class News extends \GeorgRinger\News\Domain\Model\News
-{
+class News extends \GeorgRinger\News\Domain\Model\News {
 
     /**
      * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\MiniFranske\FsMediaGallery\Domain\Model\MediaAlbum>
