1bcd2dd4a768148d9834afd3d9240819765025c5
Ruud Silvrants <ruud@beech.it>
2016-06-14 12:01:41 +0200

    [TASK] Move func to utility + cleanup unused extbase functions

--- Classes/Hooks/FolderChangedSlot.php
+++ Classes/Hooks/FolderChangedSlot.php
@@ -3,25 +3,28 @@ namespace MiniFranske\FsMediaGallery\Hooks;
 
 /***************************************************************
  *  Copyright notice
+ *
  *  (c) 2014 Frans Saris <franssaris@gmail.com>
  *  All rights reserved
+ *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
+ *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
+ *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
+ *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
 use TYPO3\CMS\Core\Resource\Folder;
-use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
-use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
  * Slots that pick up signals after (re)moving folders to update mediagallery record (sys_file_collection)
@@ -186,7 +189,7 @@ class FolderChangedSlot implements \TYPO3\CMS\Core\SingletonInterface
         $mediaFolders = $this->utilityService->getStorageFolders();
         if (count($mediaFolders)) {
             foreach ($mediaFolders as $uid => $title) {
-                $parents = $this->getFirstParentCollections($folder, $uid);
+                $parents = $this->utilityService->getFirstParentCollections($folder, $uid);
                 if (count($parents)) {
                     //take the first parent found
                     $parentUid = $parents[0]['uid'];
@@ -204,32 +207,6 @@ class FolderChangedSlot implements \TYPO3\CMS\Core\SingletonInterface
     }
 
     /**
-     * Gets the first parentCollections of the given folder and mediaFolderUid(storagepid)
-     *
-     * @param Folder $folder
-     * @param $mediaFolderUid
-     * @return array|null
-     */
-    protected function getFirstParentCollections(Folder $folder, $mediaFolderUid)
-    {
-        $parentCollection = [];
-        if ($folder->getParentFolder() == $folder) {
-            return $parentCollection;
-        } else {
-            $parentCollection = $this->utilityService->findFileCollectionRecordsForFolder(
-                $folder->getStorage()->getUid(),
-                $folder->getParentFolder()->getIdentifier(),
-                $mediaFolderUid
-            );
-            if (!count($parentCollection)) {
-                $parentCollection = $this->getFirstParentCollections($folder->getParentFolder(), $mediaFolderUid);
-            }
-        }
-
-        return $parentCollection;
-    }
-
-    /**
      * Get sub folder identifiers
      *
      * @param Folder $folder
@@ -244,31 +221,5 @@ class FolderChangedSlot implements \TYPO3\CMS\Core\SingletonInterface
         }
 
         return $folderIdentifiers;
-    }
-
-    /**
-     * Set the respectStoragePage On False, otherwise no items are found when a storagepid is required
-     *
-     * @param Repository $repository
-     * @param [] $pids
-     */
-    protected function setStoragePids(Repository $repository, $pids)
-    {
-        /** @var $querySettings Typo3QuerySettings */
-        $querySettings = $this->objectManager->get(Typo3QuerySettings::class);
-        $querySettings->setStoragePageIds($pids);
-        $repository->setDefaultQuerySettings($querySettings);
-    }
-
-    /**
-     * Persist all data that was not stored by now
-     *
-     * @return void
-     */
-    protected function persistAll()
-    {
-        $this->objectManager
-            ->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager')
-            ->persistAll();
     }
 }
--- Classes/Service/Utility.php
+++ Classes/Service/Utility.php
@@ -3,23 +3,29 @@ namespace MiniFranske\FsMediaGallery\Service;
 
 /***************************************************************
  *  Copyright notice
+ *
  *  (c) 2014 Frans Saris <franssaris@gmail.com>
  *  All rights reserved
+ *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
+ *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
+ *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
+ *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
 use \TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Resource\Folder;
 
 /**
  * Utility class
@@ -89,6 +95,32 @@ class Utility implements \TYPO3\CMS\Core\SingletonInterface
         foreach ($pagesWithPlugin as $page) {
             $this->cacheService->clearPageCache($page);
         }
+    }
+
+    /**
+     * Gets the first parentCollections of the given folder and mediaFolderUid(storagepid)
+     *
+     * @param Folder $folder
+     * @param $mediaFolderUid
+     * @return array|null
+     */
+    public function getFirstParentCollections(Folder $folder, $mediaFolderUid)
+    {
+        $parentCollection = [];
+        if ($folder->getParentFolder() == $folder) {
+            return $parentCollection;
+        } else {
+            $parentCollection = $this->findFileCollectionRecordsForFolder(
+                $folder->getStorage()->getUid(),
+                $folder->getParentFolder()->getIdentifier(),
+                $mediaFolderUid
+            );
+            if (!count($parentCollection)) {
+                $parentCollection = $this->getFirstParentCollections($folder->getParentFolder(), $mediaFolderUid);
+            }
+        }
+
+        return $parentCollection;
     }
 
     /**
