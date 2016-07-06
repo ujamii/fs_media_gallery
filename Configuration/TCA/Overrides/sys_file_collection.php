ff302f70bd94ab1beae3e496c81d29c172efad2a
macjohnny <estebanmarin@gmx.ch>
2016-07-06 18:16:40 +0000

    add typo3 6.2 compatibility

--- Configuration/TCA/Overrides/sys_file_collection.php
+++ Configuration/TCA/Overrides/sys_file_collection.php
@@ -103,6 +103,11 @@ $GLOBALS['TCA']['sys_file_collection']['ctrl']['default_sortby'] = 'ORDER BY sor
 // enable main asset preview in list module
 $GLOBALS['TCA']['sys_file_collection']['ctrl']['thumbnail'] = 'main_asset';
 
+// Compatibility with 6.2
+if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version()) < 7000000) {
+    $$additionalColumns['webdescription']['config']['wizards']['RTE']['icon'] = 'wizard_rte2.gif';
+}
+
 \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule(
     $GLOBALS['TCA']['sys_file_collection']['columns'],
     $additionalColumns
