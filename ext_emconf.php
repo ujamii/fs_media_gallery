705790ed225d0c4b2e3fab16b9085d0d7d609f59
Frans Saris <frans@beech.it>
2016-09-29 08:59:58 +0200

    [TASK] Raise verion to 1.4.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,11 +20,11 @@ Show your media assets from your local or remote storage as a gallery of albums.
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.3.2-dev',
+    'version' => '1.4.0-dev',
     'constraints' => array(
         'depends' => array(
             'php' => '5.5',
-            'typo3' => '6.2.14 - 7.6.99',
+            'typo3' => '6.2.14 - 8.3.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
