01305bdfb6ac117948857ccd19c51e324c78072c
Frans Saris <frans@beech.it>
2016-10-19 21:41:48 +0200

    [TASK] Update version to 1.4.2-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ Show your media assets from your local or remote storage as a gallery of albums.
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.4.1',
+    'version' => '1.4.2-dev',
     'constraints' => array(
         'depends' => array(
             'php' => '5.5',
