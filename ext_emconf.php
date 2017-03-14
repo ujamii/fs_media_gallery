af7fafea3a5d8fa0bcc3026c44db63c54fc8f86b
Frans Saris <franssaris@gmail.com>
2017-03-14 21:21:09 +0100

    [TASK] Raise version to 1.4.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ Show your media assets from your local or remote storage as a gallery of albums.
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.4.4',
+    'version' => '1.4.5',
     'constraints' => [
         'depends' => [
             'php' => '5.5',
