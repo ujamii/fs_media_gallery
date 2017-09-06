248040888ff4d47d358288463fbd08a856958683
Frans Saris <frans@beech.it>
2017-09-06 15:41:49 +0200

    [TASK] Set version to 1.4.6-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ Show your media assets from your local or remote storage as a gallery of albums.
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '1.4.5',
+    'version' => '1.4.6-dev',
     'constraints' => [
         'depends' => [
             'php' => '5.5',
