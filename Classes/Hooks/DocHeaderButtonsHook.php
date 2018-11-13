19c888561fa5889975c375d15eb9a654ed3b74ef
nebrot <torben1987@web.de>
2018-11-13 08:30:13 +0000

    [TASK] Remove undefined method GeneralUtility::compat_version

--- Classes/Hooks/DocHeaderButtonsHook.php
+++ Classes/Hooks/DocHeaderButtonsHook.php
@@ -66,7 +66,7 @@ class DocHeaderButtonsHook extends \MiniFranske\FsMediaGallery\Service\AbstractB
      */
     protected function createLink($title, $shortTitle, $icon, $url, $addReturnUrl = true)
     {
-        if (!GeneralUtility::compat_version('7.4')) {
+        if (version_compare(TYPO3_branch, '7.4', '<')) {
             if (strpos($url, 'alert') === 0) {
                 $url = 'javascript:' . $url;
             }
